53c5fd90426d270e47debc19f41cc3be527804e3
gilsuk <devgilsuk@gmail.com>
2025-02-25 23:04:20 +0900

    Initial commit

---
+++ src/main/java/portfolio/dauntless/DauntlessApplication.java
@@ -0,0 +1,13 @@
+package portfolio.dauntless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DauntlessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DauntlessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/portfolio/dauntless/DauntlessApplicationTests.java
@@ -0,0 +1,13 @@
+package portfolio.dauntless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DauntlessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
